8cfd767bcb1099da5e34a00bcb58c6b050127874
Ridwan Halim <190102hr@gmail.com>
2024-09-08 18:07:37 +0700

    social media login

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\EmailVerificationController;
+use App\Http\Controllers\Auth\SocialLoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,9 @@ Route::post('/login', [AuthController::class, 'login'])->name('register');
 Route::get('/success={email}&{token}', [EmailVerificationController::class, 'verifiedSuccess']);
 Route::post('/resend_email', [EmailVerificationController::class, 'resendEmailVerificationLink'])->name('resendEmailVerificationLink');
 Route::post('/check_verify_email', [EmailVerificationController::class, 'checkVerify'])->name('checkVerify');
+
+Route::get('/auth/{driver}', [SocialLoginController::class, 'toProvider'])->where('driver', 'github|google|facebook');
+Route::get('/callback/{driver}/login', [SocialLoginController::class, 'handleCallback'])->where('driver', 'github|google|facebook');
 
 
 Route::middleware('auth:sanctum')->group(function () {
